c302b322995ce22e14ae41adad1f5a570c033d23
Can ATAC <can.atac@gmail.com>
2015-10-05 10:18:42 +0200

    Initial Commit

---
+++ yall/yall.h
@@ -0,0 +1,19 @@
+//
+//  yall.h
+//  yall
+//
+//  Created by Can ATAC on 05/10/2015.
+//  Copyright © 2015 Can ATAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for yall.
+FOUNDATION_EXPORT double yallVersionNumber;
+
+//! Project version string for yall.
+FOUNDATION_EXPORT const unsigned char yallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <yall/PublicHeader.h>
+
+
